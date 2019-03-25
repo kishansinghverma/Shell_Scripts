ce70dc874c07e8cc9b792c29fdcd9c602a4affeb
kishansinghverma <kishansinghverma@gmail.com>
2019-03-25 08:49:13 +0000

    25/3

---
+++ cmd_para.sh
@@ -0,0 +1,7 @@
+echo 'First Para:' $0
+echo 'Second para:' $1
+echo 'Third Para:' $2
+echo 'Fourth para:' $3
+echo 'No Of Para:' $#
+echo 'Total Argurments:' $@
+echo 'Another One:' $*
---
+++ test.sh
@@ -0,0 +1,3 @@
+x=100
+echo x$xx
+echo $x$x $xx
